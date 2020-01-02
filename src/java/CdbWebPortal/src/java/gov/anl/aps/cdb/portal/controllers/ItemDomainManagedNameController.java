71c3c82069468a653568004990b1ccc02e758023
craigmcchesney <auto@craigmcc.com>
2020-01-02 15:30:08 -0700

    #448: add validate/import buttons to managed names list view

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainManagedNameController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainManagedNameController.java
@@ -23,23 +23,23 @@ import javax.inject.Named;
 @Named(ItemDomainManagedNameController.CONTROLLER_NAMED)
 @SessionScoped
 public class ItemDomainManagedNameController extends ItemController<ItemDomainManagedName, ItemDomainManagedNameFacade, ItemDomainManagedNameSettings> {
-    
+
     public class ValidateInfo {
-        
+
         public boolean valid = false;
         public String message = "";
-        
+
         public ValidateInfo(boolean res, String msg) {
             valid = res;
             message = msg;
         }
     }
-    
+
     public static final String CONTROLLER_NAMED = "itemDomainManagedNameController";
-    
+
     @EJB
-    ItemDomainManagedNameFacade itemDomainManagedNameFacade; 
-    
+    ItemDomainManagedNameFacade itemDomainManagedNameFacade;
+
     public static ItemDomainManagedNameController getInstance() {
         if (SessionUtility.runningFaces()) {
             return (ItemDomainManagedNameController) SessionUtility.findBean(ItemDomainManagedNameController.CONTROLLER_NAMED);
@@ -48,10 +48,50 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
             return null;
         }
     }
-    
+
+    /**
+     * Prepares import wizard.
+     */
+    public String prepareWizardValidateDevices() {
+//        importHelperValidateDevices.reset();
+//        ItemDomainImportWizard.getInstance().registerHelper(importHelperValidateDevices);
+//        return "/views/itemDomainCableCatalog/validateDevices?faces-redirect=true";
+        return "";
+    }
+
+    /**
+     * Prepares import wizard.
+     */
+    public String prepareWizardValidateSignals() {
+//        importHelperValidateDevices.reset();
+//        ItemDomainImportWizard.getInstance().registerHelper(importHelperValidateDevices);
+//        return "/views/itemDomainCableCatalog/validateDevices?faces-redirect=true";
+        return "";
+    }
+
+    /**
+     * Prepares import wizard.
+     */
+    public String prepareWizardImportDevices() {
+//        importHelperValidateDevices.reset();
+//        ItemDomainImportWizard.getInstance().registerHelper(importHelperValidateDevices);
+//        return "/views/itemDomainCableCatalog/validateDevices?faces-redirect=true";
+        return "";
+    }
+
+    /**
+     * Prepares import wizard.
+     */
+    public String prepareWizardImportSignals() {
+//        importHelperValidateDevices.reset();
+//        ItemDomainImportWizard.getInstance().registerHelper(importHelperValidateDevices);
+//        return "/views/itemDomainCableCatalog/validateDevices?faces-redirect=true";
+        return "";
+    }
+
     @Override
     protected ItemDomainManagedName instenciateNewItemDomainEntity() {
-        return new ItemDomainManagedName(); 
+        return new ItemDomainManagedName();
     }
 
     @Override
@@ -61,13 +101,13 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
 
     @Override
     protected ItemDomainManagedNameFacade getEntityDbFacade() {
-        return itemDomainManagedNameFacade; 
+        return itemDomainManagedNameFacade;
     }
 
     @Override
     public String getEntityTypeName() {
-        return "managedName"; 
-    } 
+        return "managedName";
+    }
 
     @Override
     public String getDisplayEntityTypeName() {
@@ -76,12 +116,12 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
 
     @Override
     public String getDefaultDomainName() {
-        return ItemDomainName.managedName.getValue(); 
+        return ItemDomainName.managedName.getValue();
     }
 
     @Override
     public boolean getEntityDisplayItemConnectors() {
-        return false; 
+        return false;
     }
 
     @Override
@@ -91,7 +131,7 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
 
     @Override
     public boolean getEntityDisplayDerivedFromItem() {
-        return false; 
+        return false;
     }
 
     @Override
@@ -116,32 +156,32 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
 
     @Override
     public boolean getEntityDisplayItemProperties() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayItemElements() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayItemsDerivedFromItem() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayItemMemberships() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayItemProject() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayItemEntityTypes() {
-        return false; 
+        return false;
     }
 
     @Override
@@ -156,33 +196,33 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
 
     @Override
     public String getStyleName() {
-        return "content"; 
+        return "content";
     }
 
     @Override
     public String getDefaultDomainDerivedFromDomainName() {
-        return null; 
-               
+        return null;
+
     }
 
     @Override
     public String getDefaultDomainDerivedToDomainName() {
-        return null; 
-    } 
-    
+        return null;
+    }
+
     private String matchNameFromList(String str, int startIndex, List<String> names) {
-        
+
         if (startIndex >= str.length()) {
             return "";
         }
-        
+
         for (String name : names) {
-            
+
             if (str.length() < name.length()) {
                 break;
-            } 
-            
-            for (int nameIndex = 0 ; nameIndex < name.length() ; nameIndex++) {
+            }
+
+            for (int nameIndex = 0; nameIndex < name.length(); nameIndex++) {
                 if (name.charAt(nameIndex) == str.charAt(startIndex + nameIndex)) {
                     if (nameIndex == name.length() - 1) {
                         // found a match
@@ -195,45 +235,45 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
         }
         return "";
     }
-    
+
     private String matchInstanceNumber(String str, int startIndex) {
-        
+
         int index = startIndex;
         String instanceNumber = "";
-        
+
         while ((index < str.length()) && (Character.isDigit(str.charAt(index)))) {
             instanceNumber = instanceNumber + str.charAt(index);
             index = index + 1;
         }
-        
+
         return instanceNumber;
     }
-    
+
     public ValidateInfo validateDeviceName(String name) {
-        
+
         String systemName = "";
         String systemInstance = "";
         List<String> subsystemNames = new ArrayList<>();
         List<String> subsystemInstances = new ArrayList<>();
         String deviceName = "";
         String deviceInstance = "";
-        
+
         String[] tokens = name.split(":");
-        
+
         if ((tokens.length != 2)) {
-            return new ValidateInfo(false, 
+            return new ValidateInfo(false,
                     "device name must include 2 parts separated by a colon");
         }
-        
+
         String systemToken = tokens[0];
         String deviceToken = tokens[1];
 
         List<String> dbSystemNames = getEntityDbFacade().getSystemNames();
         List<String> dbSubsystemNames = getEntityDbFacade().getSubsystemNames();
         List<String> dbDeviceNames = getEntityDbFacade().getDeviceNames();
-        
+
         int parseIndex = 0;
-        
+
         // match system name
         systemName = matchNameFromList(systemToken, parseIndex, dbSystemNames);
         if (systemName == "") {
@@ -242,27 +282,27 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
         } else {
             parseIndex = parseIndex + systemName.length();
         }
-        
+
         System.out.println("system: " + systemName + " index: " + parseIndex);
-        
+
         // match system instance if present
         systemInstance = matchInstanceNumber(systemToken, parseIndex);
         if (systemInstance != "") {
             parseIndex = parseIndex + systemInstance.length();
         }
-        
+
         System.out.println("instance: " + systemInstance + " index: " + parseIndex);
-        
+
         // can include multiple subsystems with instance
         while (parseIndex < systemToken.length()) {
-        
+
             // match subsystem name
             String subsystemName = matchNameFromList(systemToken, parseIndex, dbSubsystemNames);
             if (subsystemName == "") {
                 // make sure we have at least one subsystem name
                 if (subsystemNames.size() == 0) {
-                    return new ValidateInfo(false, "missing or invalid subsystem: " + 
-                            systemToken.substring(parseIndex));
+                    return new ValidateInfo(false, "missing or invalid subsystem: "
+                            + systemToken.substring(parseIndex));
                 } else {
                     break;
                 }
@@ -272,7 +312,7 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
                 parseIndex = parseIndex + subsystemName.length();
 
                 System.out.println("subsystem: " + subsystemName + " index: " + parseIndex);
-            
+
                 // match subsystem instance if present
                 String subsystemInstance = matchInstanceNumber(systemToken, parseIndex);
                 // add subsystem instance to list, even if empty so list indexes match
@@ -282,13 +322,13 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
                 }
             }
         }
-        
+
         // make sure all system token characters processed
         if (parseIndex < systemToken.length()) {
-            return new ValidateInfo(false, "extra characters after subsystem: " + 
-                    systemToken.substring(parseIndex));
+            return new ValidateInfo(false, "extra characters after subsystem: "
+                    + systemToken.substring(parseIndex));
         }
-      
+
         // now validate device token
         parseIndex = 0;
         deviceName = matchNameFromList(deviceToken, parseIndex, dbDeviceNames);
@@ -298,33 +338,33 @@ public class ItemDomainManagedNameController extends ItemController<ItemDomainMa
         } else {
             parseIndex = parseIndex + deviceName.length();
         }
-        
+
         System.out.println("device: " + deviceName + " index: " + parseIndex);
-        
+
         // match device instance if present
         deviceInstance = matchInstanceNumber(deviceToken, parseIndex);
         if (deviceInstance != "") {
             parseIndex = parseIndex + deviceInstance.length();
         }
-        
+
         System.out.println("instance: " + deviceInstance + " index: " + parseIndex);
-        
+
         // make sure all device token characters processed
         if (parseIndex < deviceToken.length()) {
-            return new ValidateInfo(false, "extra characters after device: " + 
-                    deviceToken.substring(parseIndex));
+            return new ValidateInfo(false, "extra characters after device: "
+                    + deviceToken.substring(parseIndex));
         }
-        
-        String msg = "valid system: " + systemName +
-                " instance: " + systemInstance +
-                " subsystems: " + subsystemNames +
-                " instances: " + subsystemInstances +
-                " device: " + deviceName +
-                " instance: " + deviceInstance;
-        
+
+        String msg = "valid system: " + systemName
+                + " instance: " + systemInstance
+                + " subsystems: " + subsystemNames
+                + " instances: " + subsystemInstances
+                + " device: " + deviceName
+                + " instance: " + deviceInstance;
+
         System.out.println(msg);
-        
+
         return new ValidateInfo(true, msg);
     }
-    
+
 }
